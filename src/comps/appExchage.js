ac844a3c3f4d5f6215078edfec15b83755b193d3
Lipaz Moris <lipazmoris7@gmail.com>
2022-04-16 16:28:11 +0300

    aaa

--- src/comps/appExchage.js
+++ src/comps/appExchage.js
@@ -7,7 +7,7 @@ export default function AppExchage() {
   return (
     <div>
       <div className='container px-5'>
-        <h1 className='py-2 pb-4 text-center display-1' style={{color: 'teal'}}>Exchage App</h1>
+        <h1 className='py-2 pb-4 text-center display-1' style={{color: 'green'}}>Exchage App</h1>
         <div className='pt-5'>
           <Input />
         </div>
